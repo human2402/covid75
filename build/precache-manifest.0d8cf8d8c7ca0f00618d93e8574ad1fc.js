e5aaa58718677eda6e6a57ce83c659ec5aebc351
human2402 <oleg.reymon24@outlook.com>
2020-09-22 00:23:51 +0900

    ops

---
+++ build/precache-manifest.0d8cf8d8c7ca0f00618d93e8574ad1fc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a4075ead905ee876ccf58b340e2887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3caaa0b6b3f47a96bb",
+    "url": "/static/css/main.8682977d.chunk.css"
+  },
+  {
+    "revision": "6a9b05d3ddbf6b54241f",
+    "url": "/static/js/2.bfe337bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bfe337bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3caaa0b6b3f47a96bb",
+    "url": "/static/js/main.167d15a3.chunk.js"
+  },
+  {
+    "revision": "531916b234806951197e",
+    "url": "/static/js/runtime-main.36ad6461.js"
+  },
+  {
+    "revision": "587cbb5d9396edb1ff242a6aee944159",
+    "url": "/static/media/book.587cbb5d.svg"
+  },
+  {
+    "revision": "805bb24efc12a51687a8bf9a7293e766",
+    "url": "/static/media/home.805bb24e.svg"
+  },
+  {
+    "revision": "36856d408b8ac1ce9b8b176b417c2302",
+    "url": "/static/media/info.36856d40.svg"
+  },
+  {
+    "revision": "b0f5933178902a1d17b15ce856580f3f",
+    "url": "/static/media/instaold.b0f59331.svg"
+  },
+  {
+    "revision": "bb1f2f10e9cc270d36b3df5426e5f598",
+    "url": "/static/media/map.bb1f2f10.svg"
+  },
+  {
+    "revision": "34fc11e3daf3d7b8e5af2f53ff47164f",
+    "url": "/static/media/mask.34fc11e3.svg"
+  },
+  {
+    "revision": "a75b22b4cdb7e90e4dd4f5831bed150a",
+    "url": "/static/media/shome.a75b22b4.svg"
+  },
+  {
+    "revision": "51b44512cc3acd478145e91a4b290427",
+    "url": "/static/media/space.51b44512.svg"
+  },
+  {
+    "revision": "0c649044b92340938f26082a3f2ca85d",
+    "url": "/static/media/stat.0c649044.svg"
+  },
+  {
+    "revision": "71f4ccd81410f187447e510c99212e09",
+    "url": "/static/media/stay-away.71f4ccd8.svg"
+  },
+  {
+    "revision": "f4f9ce3edd31fb44e2ae1368fa1a2e39",
+    "url": "/static/media/stay-home.f4f9ce3e.svg"
+  },
+  {
+    "revision": "3974b43c4e67aad9a2de0eb4e44bc498",
+    "url": "/static/media/triangle.3974b43c.svg"
+  },
+  {
+    "revision": "e71db7e191e63e247a0ff1e957a94cd8",
+    "url": "/static/media/virus.e71db7e1.svg"
+  },
+  {
+    "revision": "e008307d711d885ebfc0385fbf81e119",
+    "url": "/static/media/warning.e008307d.svg"
+  },
+  {
+    "revision": "7ebcc257e9fb471736f79ad7d3f90044",
+    "url": "/static/media/wash-hands.7ebcc257.svg"
+  },
+  {
+    "revision": "26818fb2f9065e67d4501b7f1bb91cd2",
+    "url": "/static/media/water.26818fb2.svg"
+  },
+  {
+    "revision": "b03699425d61234610d9c8648242562d",
+    "url": "/static/media/wear-mask.b0369942.svg"
+  },
+  {
+    "revision": "78ffaa631fc9b1bfb335152256249380",
+    "url": "/static/media/zab.78ffaa63.svg"
+  }
+]);
